017d4366708fe64b43d5a1fe2475b2f7a1484434
Taylor Want <taylor.want@gmail.com>
2017-07-26 12:43:36 +0000

    first commit

---
+++ dom-manipulation-notes.js
@@ -0,0 +1,21 @@
+//what is the DOM?
+
+//steps to manipulate an element: 
+
+// 1. make sure html has id and onclick
+// 2. write function to match onclick
+// 3. grab an html element by id
+// 4. change something about it.
+
+//changing inner HTML
+ 
+//changing an attribute
+
+//changing the style
+
+
+//creating a new HTML element and appending it to the body
+
+
+
+//appending a p5 element to the body!  
