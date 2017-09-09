3c7e1bc7319787c4be22e6bf19b00839460d239d
Connie <clei1@stuy.edu>
2017-09-09 02:34:39 +0000

    updated

--- dom-manipulation-notes.js
+++ dom-manipulation-notes.js
@@ -7,8 +7,29 @@
 // 3. grab an html element by id
 // 4. change something about it.
 
+function changePic() {
+    //grab the HTML elemebt by its id
+    var pic = document.getElementById('fred-pic')
+    //change the src attribute of the elemnt grabbed
+    pic.setAttribute('src','https://media.giphy.com/media/rjbLEWCY7tbSU/giphy.gif')
+    var des = document.getElementById('description')
+    des.innerHTML='Fred is the most fascinating animal in the world.'
+    var bod = document.getElementById('everything')
+    bod.style.background ='#A1CDF7'
+}
+
 //changing inner HTML
- 
+function lifeStory(){
+    var story = document.getElementById('lifestory')
+    story.innerHTML='Fred jumped off a boat.'
+    var newPara = document.createElement("p")
+    newPara.innerHTML="Fred didn't jump of the boat"
+    document.getElementById('everything').appendChild(newPara)
+}
+
+
+
+
 //changing an attribute
 
 //changing the style
@@ -18,4 +39,4 @@
 
 
 
-//appending a p5 element to the body!  
+//appending a p5 element to the body!
